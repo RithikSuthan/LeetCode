13db478fad6cfae08de219e67996964e53b2a182
Rithik Suthan S <72434153+RithikSuthan@users.noreply.github.com>
2024-05-23 16:17:35 +0530

    Time: 53 ms (49.92%), Space: 48.6 MB (75.79%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return(
+        {
+        toBe :function (val1)
+        {
+            if (val===val1)
+            {
+                return true;
+            }
+            throw "Not Equal"; 
+        },
+        notToBe :function (val1)
+        {
+            if (val!==val1)
+            {
+                return true;
+            }
+            throw "Equal"; 
+        }
+
+        }
+    );
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
