6800dc3750d4a10af01a73e24bfa162450be38f6
Rithik Suthan S <72434153+RithikSuthan@users.noreply.github.com>
2024-06-07 10:54:04 +0530

    Time: 212 ms (29.99%), Space: 24.9 MB (94.66%) - LeetHub

--- 0648-replace-words/0648-replace-words.py
+++ 0648-replace-words/0648-replace-words.py
@@ -4,14 +4,14 @@ class Solution:
         for i in dictionary:
             dict1[i]=i
         
-        print(dict1)
+        # print(dict1)
         ls=sentence.split(" ")
         # print(ls)
         for i in range(len(ls)):
             for ele in dict1:
                 if ele == ls[i][0:len(ele)]: 
                     ls[i]=dict1[ele]
-        print(ls)
+        # print(ls)
         result=" ".join(ls)
-        print(result)
+        # print(result)
         return result
