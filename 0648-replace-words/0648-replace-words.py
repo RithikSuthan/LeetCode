4c6f6a5ad64f0841db2a45168adacab77ca053b3
Rithik Suthan S <72434153+RithikSuthan@users.noreply.github.com>
2024-06-07 10:53:43 +0530

    Time: 224 ms (26.4%), Space: 24.9 MB (94.66%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.py
@@ -0,0 +1,17 @@
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        dict1={}
+        for i in dictionary:
+            dict1[i]=i
+        
+        print(dict1)
+        ls=sentence.split(" ")
+        # print(ls)
+        for i in range(len(ls)):
+            for ele in dict1:
+                if ele == ls[i][0:len(ele)]: 
+                    ls[i]=dict1[ele]
+        print(ls)
+        result=" ".join(ls)
+        print(result)
+        return result
