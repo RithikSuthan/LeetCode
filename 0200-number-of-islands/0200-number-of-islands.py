e2a76c7205ca45a7e67750af7391833db0e05be2
Rithik Suthan S <72434153+RithikSuthan@users.noreply.github.com>
2024-06-08 15:47:17 +0530

    Time: 337 ms (5.06%), Space: 27 MB (13.27%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,29 +1,26 @@
-class Solution(object):
-    def numIslands(self, grid):
-        visit=set()
-        directions=[
-            [1,0],[-1,0],[0,-1],[0,1]
-        ]
-        islands=0
-        def bfs(r,c,directions,grid):
-            queue=[]
-            queue.append((r,c))
-            while(queue):
-                # print(queue)
-                curr=queue.pop()
-                # print(curr)
-                visit.add(curr)
-                # print(visit)
-                for i in directions:
-                    u=curr[0]+i[0]
-                    v=curr[1]+i[1]
-                    if u>=0 and u<len(grid) and v>=0 and v<len(grid[0]) and (u,v) not in visit and grid[u][v] =='1':
-                            queue.append((u,v))
+class Solution:
+    from collections import deque
+    def numIslands(self, grid: List[List[str]]) -> int:
+        no_of_islands=0
+        directions=[[1,0],[-1,0],[0,1],[0,-1]]
+        visited=set()
+        rows=len(grid)
+        cols=len(grid[0])
 
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                if grid[row][col]=='1' and (row,col) not in visit:
-                    bfs(row,col,directions,grid)
-                    islands+=1
-                # print("next call")
-        return islands
+        def dfs(i,j):
+            print((i,j))
+            if (i,j) in visited:
+                return
+            visited.add((i,j))
+            for x,y in directions:
+                u=i+x
+                v=j+y
+                if u in range(0,rows) and v in range(0,cols) and grid[u][v]=="1" and (u,v) not in visited:
+                    dfs(u,v)
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j]=="1" and (i,j) not in visited:
+                    dfs(i,j)
+                    no_of_islands+=1
+        return no_of_islands
