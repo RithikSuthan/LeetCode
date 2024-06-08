7eeb92636f43e6c14d9d3a37c22909c783a920a4
Rithik Suthan S <72434153+RithikSuthan@users.noreply.github.com>
2024-06-08 13:06:48 +0530

    Time: 35 ms (55.54%), Space: 16.5 MB (35.64%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,32 +1,28 @@
 class Solution:
-    from itertools import product
     def letterCombinations(self, digits: str) -> List[str]:
-        dict1 = {
-            '2': "abc",
-            '3': "def",
-            '4': "ghi",
-            '5': "jkl",
-            '6': "mno",
-            '7': "pqrs",
-            '8': "tuv",
-            '9': "wxyz"
-        }
-        temp=[]
-        if len(digits)==0:
-            return ""
-        if len(digits)==1 and digits in  dict1:
-            return dict1[digits]
-        elif len(digits)==2:
-            temp=product( dict1[digits[0]],dict1[digits[1]])
-        elif len(digits)==3:
-            temp=product(dict1[digits[0]],dict1[digits[1]],dict1[digits[2]])
-        elif len(digits)==4:
-            temp=product(dict1[digits[0]],dict1[digits[1]],dict1[digits[2]],dict1[digits[3]])
-        print(temp)
+        dict1={
+                '2': ['a', 'b', 'c'],
+                '3': ['d', 'e', 'f'],
+                '4': ['g', 'h', 'i'],
+                '5': ['j', 'k', 'l'],
+                '6': ['m', 'n', 'o'],
+                '7': ['p', 'q', 'r', 's'],
+                '8': ['t', 'u', 'v'],
+                '9': ['w', 'x', 'y', 'z']
+                }
+        curr=[]
         result=[]
-        for i in temp:
-            result.append("".join(i))
-        print(result)
+        if digits=="":
+            return []
+        def backtrack(i):
+            if i==len(digits):
+                result.append("".join(curr))
+                return
+            for j in dict1[digits[i]]:
+                print(j,end="")
+                curr.append(j)
+                backtrack(i+1)
+                curr.pop()
+        backtrack(0)
+        # print(result)
         return result
-
-
