1522fec9f91ada6416c7f1c9d8a641249f0efae9
Rithik Suthan S <72434153+RithikSuthan@users.noreply.github.com>
2024-07-24 22:57:46 +0530

    Time: 96 ms (7.02%), Space: 16.8 MB (10.48%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.py
+++ 0507-perfect-number/0507-perfect-number.py
@@ -1,15 +1,15 @@
 class Solution:
+    import math
     def checkPerfectNumber(self, num: int) -> bool:
-        if num<=1:
+        if num==1:
             return False
-        result=1
-        for i in range(2,int(num**0.5)+1,1):
-            # print(i)
+        sum1=1
+        for i in range(2,int(num**0.5)+1):
             if num%i==0:
-                result+=i
-                if num//i!=i and i!=1:#avouid doble counting
-                    result+=num//i
-        if result==num:
+                sum1+=i
+                if i!=1:
+                    sum1+=num//i
+            print(i,sum1)
+        if num==sum1:
             return True
-        # print(result)
         return False
