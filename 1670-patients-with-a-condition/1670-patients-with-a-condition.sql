dec799d2aabc1bfa937a82b99f6f14847ab64644
Rithik Suthan S <72434153+RithikSuthan@users.noreply.github.com>
2024-07-26 22:03:01 +0530

    Time: 357 ms (34.78%), Space: 0B (100%) - LeetHub

--- 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
@@ -1,3 +1,3 @@
-select patient_id,patient_name ,conditions
-from patients 
-WHERE conditions regexp '(^|[[:space:]])DIAB1[0-9]*([[:space:]]|$)';
+# Write your MySQL query statement below
+select patient_id,patient_name,conditions from patients where instr(conditions,'DIAB1')
+and conditions like 'DIAB1%' or conditions like '% DIAB1%';
