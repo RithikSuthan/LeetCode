0dd2fe2abac0930791b00b033e6183e4e74a3aba
Rithik Suthan S <72434153+RithikSuthan@users.noreply.github.com>
2024-08-01 17:17:05 +0530

    Time: 39 ms (36.77%), Space: 16.6 MB (6.97%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,19 +1,18 @@
 class Solution:
-    def rob_house(self,nums):
-        if len(nums)==0:
-            return 0
-        if len(nums)==1:
-            return nums[0]
-        memo={1:nums[0],
-              2:max(nums[0],nums[1])
-             }
-        for i in range(3,len(nums)+1):
-                memo[i]=max(memo[i-1],memo[i-2]+nums[i-1])
-        return memo[len(nums)]
+    from collections import defaultdict
     def rob(self, nums: List[int]) -> int:
-        if len(nums)==0:
-            return 0
         if len(nums)==1:
             return nums[0]
-        return max(self.rob_house(nums[0:len(nums)-1]),self.rob_house(nums[1:len(nums)]))            
-            
+        memo=defaultdict()
+        def helper(i,end):
+            nonlocal memo
+            if i>=end:
+                return 0
+            if i in memo:
+                return memo[i]
+            memo[i]=max( nums[i]+helper(i+2,end),helper(i+1,end) )
+            return memo[i]
+        left=helper(0,len(nums)-1)
+        memo=defaultdict()
+        right=helper(1,len(nums))
+        return max( left,right )
