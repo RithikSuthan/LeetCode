5c4f875ee674fa10508550ff0e7f0a5ce71d4460
Rithik Suthan S <72434153+RithikSuthan@users.noreply.github.com>
2024-08-11 19:46:36 +0530

    Time: 28 ms (10.02%), Space: 21.9 MB (19.8%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int count=0;
+        for(auto it=nums.begin();it!=nums.end();)
+        {
+            if(*it==0)
+            {
+                nums.erase(it);
+                count+=1;
+            }
+            else
+            {
+                it++;
+            }
+        }
+        for (int i=0;i<count;i++)
+        {
+            nums.push_back(0);
+        }
+    }
+};
