54938910217740fd42d75582ceea7ff7afc03357
Rithik Suthan S <72434153+RithikSuthan@users.noreply.github.com>
2024-08-11 21:10:28 +0530

    Time: 5 ms (68.23%), Space: 17.5 MB (9.27%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,28 +1,27 @@
-#include<vector>
-#include<algorithm>
-#include<string>
 class Solution {
 public:
-    bool static compare(string &a,string &b)
+    static bool compare(string &a,string &b)
     {
         return a+b>b+a;
     }
     string largestNumber(vector<int>& nums) {
         vector<string> array;
-        string ans;
         for(int i=0;i<nums.size();i++)
         {
             array.push_back(to_string(nums[i]));
         }
         sort(array.begin(),array.end(),compare);
-        for(int i=0;i<array.size();i++)
-        {
-            ans=ans+array[i];
-        }
-        if (ans[0]=='0'){
-            return "0";
-        }
-        return ans;
-
+            string answer="";
+    for(auto it=array.begin();it!=array.end();it++)
+    {
+        answer+=*it;
     }
+    if(answer[0]=='0')
+    {
+        return "0";
+    }
+    return answer;
+    }
+
+
 };
