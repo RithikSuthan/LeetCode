066ea4b918688e6509fbdb1c9dcd76aefd35b171
Rithik Suthan S <72434153+RithikSuthan@users.noreply.github.com>
2024-10-06 20:02:35 +0530

    Time: 28 ms (90.37%), Space: 16.7 MB (13.04%) - LeetHub

--- 1923-sentence-similarity-iii/1923-sentence-similarity-iii.py
+++ 1923-sentence-similarity-iii/1923-sentence-similarity-iii.py
@@ -4,7 +4,7 @@ class Solution:
             return True
         ls=sentence1.split(" ")
         ls1=sentence2.split(" ")
-        if len(ls1)<len(ls):
+        if len(ls1)<len(ls):#Sentence 1 greater than sentence 2
             start=0
             start1=0
             flag=False
@@ -12,16 +12,11 @@ class Solution:
                 while (start<len(ls) and start1<len(ls1) and ls[start]==ls1[start1]):
                     start+=1
                     start1+=1
-                # print(ls," Before  ",ls1)
-                # while(not flag and start<len(ls) and ls[start] not in ls1):
                 while(not flag and start<len(ls) and ls1.count(ls[start]) < ls.count(ls[start])):
                     ls1.insert(start,ls[start])
                     start+=1
                 flag=True
-                # print(ls," ",ls1)
-            if ls1==ls:
-                return True
-        elif len(ls)<len(ls1):
+        elif len(ls)<len(ls1):#Sentence 2 greater than sentence 1
             start=0
             start1=0
             flag=False
@@ -29,12 +24,10 @@ class Solution:
                 while (start<len(ls) and start1<len(ls1) and ls[start]==ls1[start1]):
                     start+=1
                     start1+=1
-                # print(ls," Before  ",ls1)
                 while(not flag and start1<len(ls1) and ls.count(ls1[start1]) < ls1.count(ls1[start1])):
                     ls.insert(start1,ls1[start1])
                     start1+=1
                 flag=True
-                # print(ls," ",ls1)
-            if ls1==ls:
-                return True
+        if ls1==ls:
+            return True
         return False
