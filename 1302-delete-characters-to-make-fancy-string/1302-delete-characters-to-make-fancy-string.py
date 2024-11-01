e7b55e79f8d8689108d5dd049711576e0d388aaf
Rithik Suthan S <72434153+RithikSuthan@users.noreply.github.com>
2024-11-01 11:21:15 +0530

    Time: 312 ms (57.66%), Space: 18.3 MB (83.28%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
@@ -1,7 +1,6 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
         str1=""
-        prev=""
         for ele in s:
             if len(str1)>=2 and str1[-1]==ele and str1[-2]==ele:                    
                 pass
