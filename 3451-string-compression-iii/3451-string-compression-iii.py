8506a934582dc5a41c7710f788b3202a2092afb1
Rithik Suthan S <72434153+RithikSuthan@users.noreply.github.com>
2024-11-04 11:17:23 +0530

    Time: 363 ms (19.86%), Space: 20.5 MB (30.37%) - LeetHub

---
+++ 3451-string-compression-iii/3451-string-compression-iii.py
@@ -0,0 +1,25 @@
+class Solution:
+    from collections import defaultdict
+    def compressedString(self, word: str) -> str: 
+        ls=[]
+        i=0
+        curr=""
+        while(i<len(word)):
+            if  curr!="" and curr[-1]!=word[i]:
+                ls.append(curr)
+                curr=""
+            curr+=word[i]
+            i+=1
+        ls.append(curr)
+        # print(ls)
+
+        ans=""
+        for ele in ls:
+            cou=ele.count(ele[0])
+            while(cou>=9):
+                ans+="9"+ele[0]
+                cou-=9
+            if cou>0:
+                ans+=str(cou)+ele[0]
+            # print(ans)
+        return ans
