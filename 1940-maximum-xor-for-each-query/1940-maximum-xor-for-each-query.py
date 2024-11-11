22df565b0b12e3f3d86f4c5d868d0cdc02f7bff0
Rithik Suthan S <72434153+RithikSuthan@users.noreply.github.com>
2024-11-11 17:20:41 +0530

    Time: 33 ms (84.49%), Space: 35.8 MB (7.49%) - LeetHub

--- 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.py
+++ 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.py
@@ -1,13 +1,12 @@
 class Solution:
     def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
         max_val=2**maximumBit-1
-        print(max_val)
 
         total_xor=[nums[0]]
         for i in range(1,len(nums)):
             total_xor.append(total_xor[-1]^nums[i])
         total_xor=total_xor[::-1]
-        print(total_xor)
+
         ans=[]
         for i in range(0,len(nums)):
             ans.append(max_val^total_xor[i])
