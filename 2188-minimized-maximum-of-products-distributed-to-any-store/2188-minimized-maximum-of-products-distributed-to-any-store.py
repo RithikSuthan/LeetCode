668fd8beb87bb2b90ad097ea09ebbf35bdb38cd8
Rithik Suthan S <72434153+RithikSuthan@users.noreply.github.com>
2024-11-16 15:10:32 +0530

    Time: 541 ms (83.41%), Space: 28.7 MB (50.25%) - LeetHub

--- 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.py
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.py
@@ -1,41 +1,19 @@
 class Solution:
-    def can_distribute(self, x: int, quantities: List[int], n: int) -> bool:
-        # Pointer to the first not fully distributed product type
-        j = 0
-        # Remaining quantity of the jth product type
-        remaining = quantities[j]
-
-        # Loop through each store
-        for i in range(n):
-            # Check if the remaining quantity of the jth product type
-            # can be fully distributed to the ith store
-            if remaining <= x:
-                # If yes, move the pointer to the next product type
-                j += 1
-                # Check if all products have been distributed
-                if j == len(quantities):
-                    return True
-                else:
-                    remaining = quantities[j]
-            else:
-                # Distribute the maximum possible quantity (x) to the ith store
-                remaining -= x
-
-        return False
-
+    import math
     def minimizedMaximum(self, n: int, quantities: List[int]) -> int:
-        # Initialize the boundaries of the binary search
-        left = 0
-        right = max(quantities)
-
-        # Perform binary search until the boundaries converge
-        while left < right:
-            middle = (left + right) // 2
-            if self.can_distribute(middle, quantities, n):
-                # Try for a smaller maximum
-                right = middle
+        def canDistribute(val):
+            temp=0
+            for ele in quantities:
+                temp+=ceil(ele/val)
+            print(val,temp)
+            return temp<=n
+        l=1
+        h=max(quantities)
+        while(l<h):
+            mid=(l+h)//2
+            if canDistribute(mid):
+                h=mid
             else:
-                # Increase the minimum possible maximum
-                left = middle + 1
-
-        return left
+                l=mid+1
+        return l
+            
