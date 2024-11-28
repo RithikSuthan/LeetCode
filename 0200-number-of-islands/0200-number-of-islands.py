3092a2f17b5d7ed0945ff66e96135fcbeb5bdbc2
Rithik Suthan S <72434153+RithikSuthan@users.noreply.github.com>
2024-11-28 17:42:47 +0530

    Time: 324 ms (5.21%), Space: 28 MB (8.23%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,18 +2,13 @@ class Solution:
     from collections import deque
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        def dfs(u,v):
-            queue = deque()
-            queue.append((u,v))
-
-            while queue:
-                curr = queue.popleft()
-                visited.add(curr)
-                for u,v in directions:
-                    x = curr[0]+u
-                    y = curr[1]+v
-                    if x in range(0,rows) and y in range(0,cols) and grid[x][y]=="1" and (x,y) not in queue and (x,y) not in visited:
-                        queue.append((x,y))
+        def dfs(i,j):
+            visited.add((i,j))
+            for u,v in directions:
+                x = i+u
+                y = j+v
+                if x in range(0,rows) and y in range(0,cols) and grid[x][y]=="1"  and (x,y) not in visited:
+                    dfs(x,y)
 
 
         
