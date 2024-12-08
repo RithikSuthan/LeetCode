89266e5b1ce4712b833ebf98ced5bc67764ec074
Rithik Suthan S <72434153+RithikSuthan@users.noreply.github.com>
2024-12-08 22:14:10 +0530

    Time: 259 ms (37.38%), Space: 73.2 MB (14.41%) - LeetHub

---
+++ 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxTwoEvents(self, events: List[List[int]]) -> int:
+        ls = []
+        for ele in events:
+            ls.append([ele[0],1,ele[2]])
+            ls.append([ele[1] + 1,-1,ele[2]])
+        ls.sort()
+        # print(ls)
+        maxSeen = 0
+        maxSum = 0
+
+        for ele in ls:
+            if ele[1] == 1:
+                maxSum = max(maxSum , ele[2] + maxSeen)
+            else:
+                maxSeen = max(maxSeen , ele[2])
+        return maxSum
