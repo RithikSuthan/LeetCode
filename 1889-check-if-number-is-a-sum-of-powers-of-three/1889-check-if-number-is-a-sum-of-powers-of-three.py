fb294694d3d7a055e5d7f1a7db1389daac9731d5
Rithik Suthan S <72434153+RithikSuthan@users.noreply.github.com>
2025-03-06 22:32:04 +0530

    Time: 0 ms (100%), Space: 17.7 MB (60.63%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.py
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,19 +1,15 @@
+import bisect
 class Solution:
     def checkPowersOfThree(self, n: int) -> bool:
-        ls=[1]
-        while(ls[-1]*3 <=n):
-            ls.append(ls[-1]*3)
-        # print(ls)
-        while(n>0):
-            while( ls and ls[-1] >n):
-                ls.pop()
-            if not ls:
-                break
-            if ls:
-                n-=ls[-1]
-                ls.pop()
-            # print(ls)
+        arr = [1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147, 531441, 1594323, 4782969, 14348907, 43046721]
+        while(n > 0):
+            # print(n, " ",arr)
+            val = bisect.bisect_right(arr,n) - 1
+            n -= arr[val]
+            arr.pop(val)
             if n==0:
                 return True
         return False
+
         
+
